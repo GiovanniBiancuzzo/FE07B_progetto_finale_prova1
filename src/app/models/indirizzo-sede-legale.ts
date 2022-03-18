d74a89a7bcf7e53b6074b7fc8514d3570262b0c4
Giovanni <giovannibiancuzzo@protonmail.com>
2022-03-18 01:27:03 +0100

    signup

---
+++ src/app/models/indirizzo-sede-legale.ts
@@ -0,0 +1,10 @@
+//import { Comune } from './Comune';
+
+export interface IndirizzoSedeLegale {
+  id: number;
+  via: string;
+  civico: number;
+  cap: number;
+  localita: string;
+  //comune: Comune;
+}
--- src/app/pages/crea-fattura/crea-fattura.page.ts
+++ src/app/pages/crea-fattura/crea-fattura.page.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 
 @Component({
+  selector: 'crea-fattura-page',
   templateUrl: './crea-fattura.page.html',
   styleUrls: ['./crea-fattura.page.scss']
 })
 export class CreaFatturaPage implements OnInit {
+
+  @Input() clienteId!: number;
 
   constructor() { }
 
