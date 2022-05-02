7be1a0d64fa300d0246490cf8ec435f850467143
Giovanni Biancuzzo <giovannibiancuzzo@protonmail.com>
2022-05-02 20:46:18 +0200

    token illimitato + fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: false,
-  pathApi:'//epicode.online/epicodebeservice_v2',
-  myToken: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY0OTg1OTA2NywiZXhwIjoxNjUyMDA2NTUxfQ.NHq0u9QOpiDzPLicuTIRaFKK-Fupuu5sSBvaP-u6b6HKnkvOm-iERow-eYWDvVbcWFyFGHRh6TV5KLXyog_kJQ',
-  tenantID: 'fe_0721b'
+  pathApi: '//epicode.online/epicodebeservice_v2',
+  myToken:
+    'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY0OTg1OTA2NywiZXhwIjoxNjUyMDA2NTUxfQ.NHq0u9QOpiDzPLicuTIRaFKK-Fupuu5sSBvaP-u6b6HKnkvOm-iERow-eYWDvVbcWFyFGHRh6TV5KLXyog_kJQ',
+  tenantID: 'fe_0721b',
 };
 
 /*
